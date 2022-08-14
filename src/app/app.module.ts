b053aced65089c894ba7a960e49cffcdd837fe6b
José Rodríguez <jose.rodriguezgarcia@enreach.com>
2022-08-14 13:25:04 +0200

    Add friend form inial html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AddFriendComponent } from './add-friend/add-friend.component';
 import { AddExpenseComponent } from './add-expense/add-expense.component';
 import { BalanceComponent } from './balance/balance.component';
 import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
