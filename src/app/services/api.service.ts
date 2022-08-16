73bd964edfa69a73d38c12b4c82f56d2901ca5a4
José Rodríguez <jose.rodriguezgarcia@enreach.com>
2022-08-16 15:28:29 +0200

    Add css

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,66 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { Balance } from '../model/balance';
+import { Expense, Expenses, NewExpense } from '../model/expenses';
+import { Friend, Friends, NewFriend } from '../model/friend';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private expensesUrl = 'http://host.docker.internal:8080'
+
+  constructor(private http: HttpClient) { }
+
+  getExpenses(): Observable<Expenses> {
+    return this.http.get<Expenses>(`${this.expensesUrl}/expenses`).pipe(
+      tap(expenses => console.log('fetched', expenses)),
+      catchError((error: any) => {
+        console.error(error)
+        return of({ expenses: [] })
+      })
+    )
+  }
+
+  addExpense(newExpense: NewExpense): Observable<Expense | null> {
+    return this.http.post<Expense | null>(`${this.expensesUrl}/expenses`, newExpense).pipe(
+      tap(expense => console.log('fetched', expense)),
+      catchError((error: any) => {
+        console.error(error)
+        return of(null)
+      })
+    )
+  }
+
+  getFriends(): Observable<Friends> {
+    return this.http.get<Friends>(`${this.expensesUrl}/friends`).pipe(
+      tap(friends => console.log('fetched', friends)),
+      catchError((error: any) => {
+        console.error(error)
+        return of({ friends: [] })
+      })
+    )
+  }
+
+  addFriend(newFriend: NewFriend): Observable<Friend | null> {
+    return this.http.post<Friend | null>(`${this.expensesUrl}/friends`, newFriend).pipe(
+      tap(friend => console.log('fetched', friend)),
+      catchError((error: any) => {
+        console.error(error)
+        return of(null)
+      })
+    )
+  }
+
+  getBalance(): Observable<Balance> {
+    return this.http.get<Balance>(`${this.expensesUrl}/balance`).pipe(
+      tap(balance => console.log('fetched', balance)),
+      catchError((error: any) => {
+        console.error(error)
+        return of({ balanceItems: [] })
+      })
+    )
+  }
+
+}
